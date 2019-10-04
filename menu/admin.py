ef2cf12d0000f3c99579dffff712bcbc9b756c41
Luis Palacios <lrpalaciosdev@gmail.com>
2019-10-04 16:44:01 -0600

    add menu items and tags

--- menu/admin.py
+++ menu/admin.py
@@ -1,6 +1,16 @@
+from django import forms
 from django.contrib import admin
 from django.utils.html import format_html
-from .models import MenuCategory
+from ckeditor.widgets import CKEditorWidget
+from .models import MenuCategory, MenuItem, MenuTag
+
+
+class MenuItemAdminForm(forms.ModelForm):
+    description = forms.CharField(widget=CKEditorWidget())
+    class Meta:
+        model = MenuItem
+        fields = '__all__'
+
 
 @admin.register(MenuCategory)
 class MenuCategoryAdmin(admin.ModelAdmin):
@@ -13,3 +23,39 @@ class MenuCategoryAdmin(admin.ModelAdmin):
         return format_html('<img src="{}" style="width:150px" />'.format(obj.image.url))
 
     image_tag.short_description = 'Image'
+
+@admin.register(MenuTag)
+class MenuTagAdmin(admin.ModelAdmin):
+    list_display = ['name', 'icon', 'icon_tag']
+
+    def icon_tag(self, obj):
+        return format_html('<i class="{}" />'.format(obj.icon))
+
+    class Media:
+        js = ('https://kit.fontawesome.com/6274f95d8c.js', )
+
+
+@admin.register(MenuItem)
+class MenuItemAdmin(admin.ModelAdmin):
+    form = MenuItemAdminForm
+    list_display = ['name', 'category', 'order', 'tags_icons', 'image_tag', 'price', 'active']
+    list_editable = ['order', ]
+    ordering = ['category__order',  'order', ]
+    list_filter = ['active', 'category', 'tags']
+    filter_horizontal = ['tags']
+
+    def image_tag(self, obj):
+        return format_html('<img src="{}" style="width:150px" />'.format(obj.image.url))
+
+    def tags_icons(self, obj):
+        tags = obj.tags.all()
+        icons = ''
+        for tag in tags:
+            icons += ' <i class="{}" title="{}"></i>'.format(tag.icon, tag.name)
+        return format_html(icons)
+
+    class Media:
+        js = ('https://kit.fontawesome.com/6274f95d8c.js', )
+
+    image_tag.short_description = 'Image'
+    tags_icons.short_description = 'Tags'
--- menu/models.py
+++ menu/models.py
@@ -3,9 +3,34 @@ from django.db import models
 class MenuCategory(models.Model):
     name = models.CharField(max_length=250)
     order = models.IntegerField()
-    image = models.ImageField()
+    image = models.ImageField(upload_to='categories/')
     active = models.BooleanField(default=True)
 
     def __str__(self):
         return self.name
 
+
+class MenuTag(models.Model):
+    name = models.CharField(max_length=200)
+    icon = models.CharField(max_length=50,
+                            help_text='<a href="https://fontawesome.com/icons?d=gallery&m=free" target="_blank">Check available icons</a>')
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
+
+class MenuItem(models.Model):
+    name = models.CharField(max_length=250)
+    order = models.IntegerField()
+    image = models.ImageField(upload_to='items/')
+    active = models.BooleanField(default=True)
+    description = models.TextField(null=True, blank=True)
+    price = models.FloatField()
+    category = models.ForeignKey(MenuCategory,
+                                 on_delete=models.CASCADE,
+                                 limit_choices_to={'active': True},)
+    tags = models.ManyToManyField(MenuTag, limit_choices_to={'active': True})
+
+    def __str__(self):
+        return self.name
