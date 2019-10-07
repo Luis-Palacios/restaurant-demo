800ad8bccef9dbd3313f93c4d702edd81bf2c990
Luis Palacios <lrpalaciosdev@gmail.com>
2019-10-07 15:58:41 -0600

    initial show of home page

--- menu/views.py
+++ menu/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import MenuItem
+from config.models import GeneralInfo
 
-# Create your views here.
+def index(request):
+    general_info = GeneralInfo.objects.first()
+    menu_items = MenuItem.objects.order_by('-category__name', 'order').filter(active=True)
+    return render(request, 'menu/menu.html', {
+        'general_info': general_info,
+        'menu_items': menu_items
+    })
